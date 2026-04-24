3dbb370e3ee4afb268a44e5a59ffb39ac966d6d7
Graziela <grazielaguimaraes1988.24@gmail.com>
2026-04-23 21:50:31 -0300

    Testando como pegar o clique do teclado do usuário

--- calculadora.js
+++ calculadora.js
@@ -1,27 +1,133 @@
 
+    
+
+   //Verifica a ação no teclado usuário
+    document.addEventListener("keydown", function(event) {
+     let teclaSelecionada =  event.key
+     console.log(teclaSelecionada)
+   });
+
+  
 /*Calculadora*/
+function cal(tipo, valor) {
+
+ 
 
 
-
-function cal(tipo, valor){
-
-   //Estpa dando erro
+    console.log(tipo, valor)
+    //Estpa dando erro
     //Verificando os valores vindos por parâmentro
-    if(tipo == 'numero') {
 
-        switch(valor) {
+    if (tipo == 'numero') {
 
-            
+        switch (valor) {
+
+
             case '1':
-            document.getElementById('tela').value = '1'
-            break
+                document.getElementById('tela').value += '1'
+                break
+
+            case '2':
+                document.getElementById('tela').value += '2'
+                break
+
+            case '3':
+                document.getElementById('tela').value += '3'
+                break
+
+            case '4':
+                document.getElementById('tela').value += '4'
+                break
+
+            case '5':
+                document.getElementById('tela').value += '5'
+                break
+
+            case '6':
+                document.getElementById('tela').value += '6'
+                break
+
+            case '7':
+                document.getElementById('tela').value += '7'
+                break
+
+            case '8':
+                document.getElementById('tela').value += '8'
+                break
+
+            case '9':
+                document.getElementById('tela').value += '9'
+                break
+
+            case '0':
+                document.getElementById('tela').value += '0'
+                break
 
             default:
                 tela.value = 'Erro'
         }
-    } 
+    }
 
-    else{
-        alert('erro') //Faz algo caso seja um operador
+    else if (tipo == 'outro') {
+
+        switch (valor) {
+
+            case '+':
+                document.getElementById('tela').value += '+'
+                break
+
+            case '-':
+                document.getElementById('tela').value += '-'
+                break
+
+                 case '*':
+             document.getElementById('tela').value += '*'
+            break
+
+             case '/':
+             document.getElementById('tela').value += '/'
+            break
+
+             case '.':
+             document.getElementById('tela').value += '.'
+            break
+
+             case ',':
+             document.getElementById('tela').value += ','
+            break
+
+            //verificar
+             case 'del':
+             let num = document.getElementById('tela').value
+             let deletar = num.slice(0,-1)
+             document.getElementById('tela').value = deletar
+             break //Se não colcar o break vai dar tudo errado
+            
+            //verificar
+            case 'expo':
+             document.getElementById('tela').value = document.getElementById('tela').value ** 2//? Verificar como fazer
+            break
+
+
+
+            case '=':
+            let telaRes = document.getElementById('tela').value 
+            let resultado = eval(telaRes)
+
+            document.getElementById('tela'). value = resultado
+
+            break
+
+             
+            case 'limpar':
+             document.getElementById('tela').value =''
+             break
+
+            default:
+                document.getElementById('tela').value = 'Erro desconhecido'
+            
+
+        }
+
     }
 }
