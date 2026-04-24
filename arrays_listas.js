e617a82ecb356bbb35a19f5702b9f0e2063f8e7d
Graziela <grazielaguimaraes1988.24@gmail.com>
2026-04-23 22:38:02 -0300

    Exercício proposto - aula 20 - SCTEC

--- arrays_listas.js
+++ arrays_listas.js
@@ -1,16 +1,25 @@
+//Criar um array de frutas
+let frutas = []
 
-function adicionar() {
+//Inserir cinco frutas
+frutas.push('Mação', 'Banana', 'Carambola', 'Amora', 'Ameixa')
+console.log(frutas)
 
-    
-let produto = document.getElementById('produto').value 
+//acessar o terceiro item e exibi-lo
+let terceiro_item = frutas[2] 
+let pegar_indice = frutas[3 - 1] 
+console.log(`Terceiro ítem do array: ${pegar_indice}`)
 
-let lista = []
-lista.push(produto)
+//alterar o segundo ítem do array
+frutas[1] = 'Manga'
+console.log(frutas)
 
-for(let p in lista) {
+frutas.push('Laranja', 'Pêssego')
+console.log(frutas)
 
-console.log(lista[p])
-}
+//Tamanho do array
+let tamanho_length = frutas.length - 1
+console.log(tamanho_length)
 
 
-}
+
